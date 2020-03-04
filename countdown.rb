181821b455fefdc68b58ed274ef26662779d14c8
Peter Rose <peterroseq1@gmail.com>
2020-03-04 17:35:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,9 @@ number = 11
 def countdown(number)
   
   while number > 0
-    number -= 1  
+     
       puts "#{number} SECOND(S)!"
+        number -= 1 
     end
        return "HAPPY NEW YEAR!" 
 end

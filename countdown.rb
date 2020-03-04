5cb60f3aa79ba68690ff54fb406893c33bb330e0
Peter Rose <peterroseq1@gmail.com>
2020-03-04 17:30:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown(number)
    number = 11
   while number > 0
     number -= 1  
-      puts "+#{number} SECOND(S)!"
+      puts "#{number} SECOND(S)!"
     end
        return "HAPPY NEW YEAR!" 
 end
@@ -12,7 +12,7 @@ def countdown_with_sleep(number)
   while number > 0 
     sleep(0.5)
     number -= 1 
-      puts "+#{number} SECOND(S)!"
+      puts "#{number} SECOND(S)!"
     end
        return "HAPPY NEW YEAR!"
 end
